f099c4a59d2b6f3ab33d99c29e960c0f6867b5c9
ArtyomLobanov <lobanov98@yandex.ru>
2017-11-27 11:35:30 +0300

    Add build instructions

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,5 +1,6 @@
 package ru.spbau.mit
 
+import com.intellij.ui.components.JBScrollPane
 import org.antlr.v4.gui.TreeViewer
 import org.antlr.v4.runtime.BufferedTokenStream
 import org.antlr.v4.runtime.CharStreams
@@ -9,22 +10,17 @@ import ru.spbau.mit.ast.InterpreterException
 import ru.spbau.mit.parser.LLanguageLexer
 import ru.spbau.mit.parser.LLanguageParser
 import javax.swing.JFrame
-import javax.swing.JPanel
 
 
 fun main(args: Array<String>) {
-    if (args.size != 2) {
-        print("Exactly two argument expected (path to file and flag - show tree or not)!")
+    if (args.size != 1) {
+        print("Exactly one argument expected (path to file)!")
         return
     }
     try {
         val tree = ASTBuilder.buildAST(CharStreams.fromFileName(args[0]))
         val visitor = ASTPrinter(System.out)
         tree.root.visit(visitor)
-        val flag = args[1].toBoolean()
-        if (flag) {
-            showTree(args[0])
-        }
     } catch (e: Exception) {
         print("Something went wrong:\n")
         print("Message: ${e.message}\n")
@@ -39,10 +35,9 @@ fun showTree(filename: String) {
     val parser = LLanguageParser(BufferedTokenStream(lexer))
     parser.buildParseTree = true
     val frame = JFrame("Antlr AST")
-    val panel = JPanel()
     val viewer = TreeViewer(parser.ruleNames.asList(), parser.file())
+    val panel = JBScrollPane(viewer)
     viewer.scale = 1.5
-    panel.add(viewer)
     frame.add(panel)
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     frame.setSize(1000, 1000)
